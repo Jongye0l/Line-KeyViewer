058ac9132bdb2a9cbcebedb606bdaf29a1529461
Jongyeol <bcpjy1233@gmail.com>
2025-05-05 01:17:46 +0900

    feat: Setting Key Swap

--- LineKeyViewer/Form2.cs
+++ LineKeyViewer/Form2.cs
@@ -62,13 +62,81 @@ namespace LineKeyViewer {
             hook.Dispose();
         }
 
+        private void ChangeKey(int fromKey, int toKey) {
+            if(app.key1 == fromKey) {
+                Properties.Settings.Default.pianoKey1 = toKey;
+                app.key1 = toKey;
+                button1.Text = ((Keys) toKey).ToString();
+            } else if(app.key2 == fromKey) {
+                Properties.Settings.Default.pianoKey2 = toKey;
+                app.key2 = toKey;
+                button2.Text = ((Keys) toKey).ToString();
+            } else if(app.key3 == fromKey) {
+                Properties.Settings.Default.pianoKey3 = toKey;
+                app.key3 = toKey;
+                button3.Text = ((Keys) toKey).ToString();
+            } else if(app.key4 == fromKey) {
+                Properties.Settings.Default.pianoKey4 = toKey;
+                app.key4 = toKey;
+                button4.Text = ((Keys) toKey).ToString();
+            } else if(app.key5 == fromKey) {
+                Properties.Settings.Default.pianoKey5 = toKey;
+                app.key5 = toKey;
+                button5.Text = ((Keys) toKey).ToString();
+            } else if(app.key6 == fromKey) {
+                Properties.Settings.Default.pianoKey6 = toKey;
+                app.key6 = toKey;
+                button6.Text = ((Keys) toKey).ToString();
+            } else if(app.key7 == fromKey) {
+                Properties.Settings.Default.pianoKey7 = toKey;
+                app.key7 = toKey;
+                button7.Text = ((Keys) toKey).ToString();
+            } else if(app.key8 == fromKey) {
+                Properties.Settings.Default.pianoKey8 = toKey;
+                app.key8 = toKey;
+                button8.Text = ((Keys) toKey).ToString();
+            } else if(app.key9 == fromKey) {
+                Properties.Settings.Default.pianoKey9 = toKey;
+                app.key9 = toKey;
+                button9.Text = ((Keys) toKey).ToString();
+            } else if(app.key10 == fromKey) {
+                Properties.Settings.Default.pianoKey10 = toKey;
+                app.key10 = toKey;
+                button10.Text = ((Keys) toKey).ToString();
+            } else if(app.key11 == fromKey) {
+                Properties.Settings.Default.pianoKey11 = toKey;
+                app.key11 = toKey;
+                button11.Text = ((Keys) toKey).ToString();
+            } else if(app.key12 == fromKey) {
+                Properties.Settings.Default.pianoKey12 = toKey;
+                app.key12 = toKey;
+                button12.Text = ((Keys) toKey).ToString();
+            } else if(app.key13 == fromKey) {
+                Properties.Settings.Default.pianoKey13 = toKey;
+                app.key13 = toKey;
+                button13.Text = ((Keys) toKey).ToString();
+            } else if(app.key14 == fromKey) {
+                Properties.Settings.Default.pianoKey14 = toKey;
+                app.key14 = toKey;
+                button14.Text = ((Keys) toKey).ToString();
+            } else if(app.key15 == fromKey) {
+                Properties.Settings.Default.pianoKey15 = toKey;
+                app.key15 = toKey;
+                button15.Text = ((Keys) toKey).ToString();
+            } else if(app.key16 == fromKey) {
+                Properties.Settings.Default.pianoKey16 = toKey;
+                app.key16 = toKey;
+                button16.Text = ((Keys) toKey).ToString();
+            }
+        }
+
         private void Settings_KeyDown(object sender, KeyEventArgs e) {
             int keyCode = e.KeyValue;
             if(e.KeyCode == Keys.Space || e.KeyCode == Keys.Return) isSpace = true;
             if(key1_trigger) {
                 key1_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key1) {
+                    ChangeKey(keyCode, app.key1);
                     Properties.Settings.Default.pianoKey1 = keyCode;
                     app.key1 = keyCode;
                     Properties.Settings.Default.Save();
@@ -76,8 +144,8 @@ namespace LineKeyViewer {
                 button1.Text = ((Keys) app.key1).ToString();
             } else if(key2_trigger) {
                 key2_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key2) {
+                    ChangeKey(keyCode, app.key2);
                     Properties.Settings.Default.pianoKey2 = keyCode;
                     app.key2 = keyCode;
                     Properties.Settings.Default.Save();
@@ -85,8 +153,8 @@ namespace LineKeyViewer {
                 button2.Text = ((Keys) app.key2).ToString();
             } else if(key3_trigger) {
                 key3_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key3) {
+                    ChangeKey(keyCode, app.key3);
                     Properties.Settings.Default.pianoKey3 = keyCode;
                     app.key3 = keyCode;
                     Properties.Settings.Default.Save();
@@ -94,8 +162,8 @@ namespace LineKeyViewer {
                 button3.Text = ((Keys) app.key3).ToString();
             } else if(key4_trigger) {
                 key4_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key4) {
+                    ChangeKey(keyCode, app.key4);
                     Properties.Settings.Default.pianoKey4 = keyCode;
                     app.key4 = keyCode;
                     Properties.Settings.Default.Save();
@@ -103,8 +171,8 @@ namespace LineKeyViewer {
                 button4.Text = ((Keys) app.key4).ToString();
             } else if(key5_trigger) {
                 key5_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key5) {
+                    ChangeKey(keyCode, app.key5);
                     Properties.Settings.Default.pianoKey5 = keyCode;
                     app.key5 = keyCode;
                     Properties.Settings.Default.Save();
@@ -112,8 +180,8 @@ namespace LineKeyViewer {
                 button5.Text = ((Keys) app.key5).ToString();
             } else if(key6_trigger) {
                 key6_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key6) {
+                    ChangeKey(keyCode, app.key6);
                     Properties.Settings.Default.pianoKey6 = keyCode;
                     app.key6 = keyCode;
                     Properties.Settings.Default.Save();
@@ -121,8 +189,8 @@ namespace LineKeyViewer {
                 button6.Text = ((Keys) app.key6).ToString();
             } else if(key7_trigger) {
                 key7_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key7) {
+                    ChangeKey(keyCode, app.key7);
                     Properties.Settings.Default.pianoKey7 = keyCode;
                     app.key7 = keyCode;
                     Properties.Settings.Default.Save();
@@ -130,8 +198,8 @@ namespace LineKeyViewer {
                 button7.Text = ((Keys) app.key7).ToString();
             } else if(key8_trigger) {
                 key8_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key8) {
+                    ChangeKey(keyCode, app.key8);
                     Properties.Settings.Default.pianoKey8 = keyCode;
                     app.key8 = keyCode;
                     Properties.Settings.Default.Save();
@@ -139,8 +207,8 @@ namespace LineKeyViewer {
                 button8.Text = ((Keys) app.key8).ToString();
             } else if(key9_trigger) {
                 key9_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key9) {
+                    ChangeKey(keyCode, app.key9);
                     Properties.Settings.Default.pianoKey9 = keyCode;
                     app.key9 = keyCode;
                     Properties.Settings.Default.Save();
@@ -148,8 +216,8 @@ namespace LineKeyViewer {
                 button9.Text = ((Keys) app.key9).ToString();
             } else if(key10_trigger) {
                 key10_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key10) {
+                    ChangeKey(keyCode, app.key10);
                     Properties.Settings.Default.pianoKey10 = keyCode;
                     app.key10 = keyCode;
                     Properties.Settings.Default.Save();
@@ -157,8 +225,8 @@ namespace LineKeyViewer {
                 button10.Text = ((Keys) app.key10).ToString();
             } else if(key11_trigger) {
                 key11_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key11) {
+                    ChangeKey(keyCode, app.key11);
                     Properties.Settings.Default.pianoKey11 = keyCode;
                     app.key11 = keyCode;
                     Properties.Settings.Default.Save();
@@ -166,8 +234,8 @@ namespace LineKeyViewer {
                 button11.Text = ((Keys) app.key11).ToString();
             } else if(key12_trigger) {
                 key12_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key12) {
+                    ChangeKey(keyCode, app.key12);
                     Properties.Settings.Default.pianoKey12 = keyCode;
                     app.key12 = keyCode;
                     Properties.Settings.Default.Save();
@@ -175,8 +243,8 @@ namespace LineKeyViewer {
                 button12.Text = ((Keys) app.key12).ToString();
             } else if(key13_trigger) {
                 key13_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key13) {
+                    ChangeKey(keyCode, app.key13);
                     Properties.Settings.Default.pianoKey13 = keyCode;
                     app.key13 = keyCode;
                     Properties.Settings.Default.Save();
@@ -184,8 +252,8 @@ namespace LineKeyViewer {
                 button13.Text = ((Keys) app.key13).ToString();
             } else if(key14_trigger) {
                 key14_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key14) {
+                    ChangeKey(keyCode, app.key14);
                     Properties.Settings.Default.pianoKey14 = keyCode;
                     app.key14 = keyCode;
                     Properties.Settings.Default.Save();
@@ -193,8 +261,8 @@ namespace LineKeyViewer {
                 button14.Text = ((Keys) app.key14).ToString();
             } else if(key15_trigger) {
                 key15_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key15) {
+                    ChangeKey(keyCode, app.key15);
                     Properties.Settings.Default.pianoKey15 = keyCode;
                     app.key15 = keyCode;
                     Properties.Settings.Default.Save();
@@ -202,8 +270,8 @@ namespace LineKeyViewer {
                 button15.Text = ((Keys) app.key15).ToString();
             } else if(key16_trigger) {
                 key16_trigger = false;
-                if(keyCode != app.key1 && keyCode != app.key2 && keyCode != app.key3 && keyCode != app.key4 && keyCode != app.key5 && keyCode != app.key6 && keyCode != app.key7 && keyCode != app.key8 &&
-                   keyCode != app.key9 && keyCode != app.key10 && keyCode != app.key11 && keyCode != app.key12 && keyCode != app.key13 && keyCode != app.key14 && keyCode != app.key15 && keyCode != app.key16) {
+                if(keyCode != app.key16) {
+                    ChangeKey(keyCode, app.key16);
                     Properties.Settings.Default.pianoKey16 = keyCode;
                     app.key16 = keyCode;
                     Properties.Settings.Default.Save();
